422dbe61a54912a5b1aad0155abe0ac7b74cf362
LariHong <gccbs05591job@gmail.com>
2024-06-28 12:55:43 +0800

    24 修正

--- features/bollinger.py
+++ features/bollinger.py
@@ -10,9 +10,13 @@ def Calculate_Bollinger_Bands(data: pd.DataFrame, window=20, num_std=2)->pd.Data
     close_mean:pd.Series=data['Close'].astype(float).mean()
     data['std_dev'] =data['std_dev'].fillna(round(close_mean,4))
     
+    data['upperband'] = round(data['ma'] + num_std * data['std_dev'], 4)
+    data['lowerband'] = round(data['ma'] - num_std * data['std_dev'], 4)
+    
     # 計算布林通道的上限線和下限線
-    data['upperband'] = round(data['ma'] + num_std * data['std_dev'],4)
-    data['lowerband'] = round(data['ma'] - num_std * data['std_dev'],4)
+    for i in range(window):
+        data.loc[i, 'upperband'] = round(data['upperband'][window:].mean(), 4)
+        data.loc[i, 'lowerband'] = round(data['lowerband'][window:].mean(), 4)
 
     
     return data
